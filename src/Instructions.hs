e0309935e4cd0d1a15d4d7f6e57a4b30804727e0
jrreed83 <joeyrreed@gmail.com>
2017-11-22 21:21:57 -0800

    jrr

--- src/Instructions.hs
+++ src/Instructions.hs
@@ -59,168 +59,48 @@ u32 :: Parser W.Word32
 u32 = integer >>= (\x -> return $ asWord32 x)
 
 -- Register Stuff
-data Reg = ZERO 
-             | AT 
-             | V0 | V1 
-             | A0 | A1 | A2 | A3 
-             | T0 | T1 | T2 | T3 | T4 | T5 | T6 | T7
-             | S0 | S1 | S2 | S3 | S4 | S5 | S6 | S7 
-             | T8 | T9 
-             | K0 | K1 
-             | GP 
-             | SP
-             | FP 
-             | RP 
-             deriving (Show, Eq)
+data Reg = X { regCode :: W.Word8 } 
+         deriving (Show, Eq)
 
--- Constant value 0
-zero :: Parser Reg 
-zero = (string "$zero") >>= (\_ -> return ZERO)
   
 -- Assembler temporary
-at :: Parser Reg
-at = (string "$at") >>= (\_ -> return AT)
-
--- 
-v0 :: Parser Reg 
-v0 = (string "$v0") >>= (\_ -> return V0)
-
-v1 :: Parser Reg 
-v1 = (string "$v1") >>= (\_ -> return V1)
-
-a0 :: Parser Reg 
-a0 = (string "$a0") >>= (\_ -> return A0)
-
-a1 :: Parser Reg 
-a1 = (string "$a1") >>= (\_ -> return A1)
-
-a2 :: Parser Reg 
-a2 = (string "$a2") >>= (\_ -> return A2)
-
-a3 :: Parser Reg 
-a3 = (string "$a3") >>= (\_ -> return A3)
-
-t0 :: Parser Reg 
-t0 = (string "$t0") >>= (\_ -> return T0)
-
-t1 :: Parser Reg 
-t1 = (string "$t1") >>= (\_ -> return T1)
-
-t2 :: Parser Reg 
-t2 = (string "$t2") >>= (\_ -> return T2)
-
-t3 :: Parser Reg 
-t3 = (string "$t3") >>= (\_ -> return T3)   
-    
-t4 :: Parser Reg 
-t4 = (string "$t4") >>= (\_ -> return T4)
-
-t5 :: Parser Reg 
-t5 = (string "$t5") >>= (\_ -> return T5)
-
-t6 :: Parser Reg 
-t6 = (string "$t6") >>= (\_ -> return T6)
-
-t7 :: Parser Reg 
-t7 = (string "$t7") >>= (\_ -> return T7)      
-
-s0 :: Parser Reg 
-s0 = (string "$s0") >>= (\_ -> return S0)
-
-s1 :: Parser Reg 
-s1 = (string "$s1") >>= (\_ -> return S1)
-
-s2 :: Parser Reg 
-s2 = (string "$s2") >>= (\_ -> return S2)
-
-s3 :: Parser Reg 
-s3 = (string "$s3") >>= (\_ -> return S3)   
-    
-s4 :: Parser Reg 
-s4 = (string "$s4") >>= (\_ -> return S4)
-
-s5 :: Parser Reg 
-s5 = (string "$s5") >>= (\_ -> return S5)
-
-s6 :: Parser Reg 
-s6 = (string "$s6") >>= (\_ -> return S6)
-
-s7 :: Parser Reg 
-s7 = (string "$s7") >>= (\_ -> return S7)  
-
-t8 :: Parser Reg 
-t8 = (string "$t8") >>= (\_ -> return T8)
-
-t9 :: Parser Reg 
-t9 = (string "$t9") >>= (\_ -> return T9)
-
 register :: Parser Reg
-register = 
-        zero <|>
-        at <|>
-        v0 <|> v1 <|>
-        a0 <|> a1 <|> a2 <|> a3 <|>
-        t0 <|> t1 <|> t2 <|> t3 <|> t4 <|> t5 <|> t6 <|> t7 <|>
-        s0 <|> s1 <|> s2 <|> s3 <|> s4 <|> s5 <|> s6 <|> s7 <|>
-        t8 <|> t9
+register = do 
+    _ <- string "x" 
+    i <- u8 
+    if i <= 31 then return $ X i else failure "Not a valid register"
+
 --------------------------------------------------------------------------------
 
-data Rop = ADD | SUB | AND | OR | XOR
-             deriving (Show,Eq)
+data Rop = ADD | SUB | AND | OR | XOR deriving (Show,Eq)
 
-data Iop = ADDI | LD | STO 
-             deriving (Show,Eq)
+data Iop = ADDI | LD | STO deriving (Show,Eq)
 
-data Immed = Immed W.Word32 
-               deriving (Show, Eq)
+data Immed = Immed W.Word32 deriving (Show, Eq)
 
 data Shft = Shft W.Word8 
-               deriving (Show, Eq)
+            deriving (Show, Eq)
 
     -- Instruction Set 
 data Instruction = RType Rop Reg Reg Reg W.Word8
-                     | IType Iop Reg Reg     W.Word32
-                     | NOP 
-                     deriving (Show, Eq)
+                 | IType Iop Reg Reg     W.Word32
+                 | NOP 
+                 deriving (Show, Eq)
 
 add :: Parser Instruction 
 add = do
-        _  <- string "add"
-        _  <- spaces 
-        rd <- register
-        _  <- comma 
-        rt <- register 
-        _  <- comma 
-        rs <- register 
-        return $ RType ADD rd rt rs 0
+    _  <- string "add"
+    _  <- spaces 
+    rd <- register
+    _  <- comma 
+    rt <- register 
+    _  <- comma 
+    rs <- register 
+    return $ RType ADD rd rt rs 0
 
                                     
-    ----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 
-regCode :: Reg -> W.Word8
-regCode ZERO = 0
-regCode AT   = 1 
-regCode V0   = 2
-regCode V1   = 3 
-regCode A0   = 4
-regCode A1   = 5 
-regCode A3   = 6
-regCode T0   = 7 
-regCode T1   = 8 
-regCode T2   = 9 
-regCode T3   = 10 
-regCode T4   = 11 
-regCode T5   = 12 
-regCode T6   = 13 
-regCode T7   = 14
-regCode S0   = 15 
-regCode S1   = 16 
-regCode S2   = 17 
-regCode S3   = 18 
-regCode S4   = 19 
-regCode S5   = 20 
-regCode S6   = 21 
-regCode S7   = 22    
     
 funCode :: Rop -> W.Word8 
 funCode _ = 0
