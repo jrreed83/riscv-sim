4f8c8e151f3cf8c7a18c98bdbad8c6a8480af26f
jrreed83 <joeyrreed@gmail.com>
2017-11-29 06:15:32 -0800

    jrr

--- src/ParserLib.hs
+++ src/ParserLib.hs
@@ -18,89 +18,112 @@ module ParserLib
     , anyString
     , label
     , choice
+--    , Result'(..)
     ) where
         
 
-data Result a = Success {match :: a, rest :: String}
-              | Failure {msg :: String}
-              deriving (Show, Eq)
 
-data Parser a = Parser { run :: String -> Result a }
+data Result b a = Success {match :: a, rest :: b}
+                | Failure {msg :: String}
+                deriving (Show, Eq)
 
-parse :: Parser a -> String -> Result a 
+data Parser b a = Parser {run :: b -> Result b a }
+
+--type Parser = GenParser String 
+--type Result = GenResult String 
+
+--data Result a = Success {match :: a, rest :: String}
+--              | Failure {msg :: String}
+--              deriving (Show, Eq)
+
+--data Parser a = Parser { run :: String -> Result a }
+
+parse :: Parser String a -> String -> Result String a 
 parse pa str = case run pa str of 
                     Failure msg  -> Failure msg 
                     Success x "" -> Success x ""
                     Success x r  -> Failure "Failed to parse entire"
 
-instance Functor Parser where
-    fmap f pa = Parser $ 
-                    \s -> case run pa s of 
-                               Failure m   -> Failure m 
-                               Success x c -> Success (f x) c        
+instance Functor (Parser b) where
+    fmap f pa = Parser $ \s -> 
+        case run pa s of 
+             Failure m   -> Failure m 
+             Success x c -> Success (f x) c 
 
-instance Applicative Parser where 
+--instance Functor Parser where
+--    fmap f pa = Parser $ \s -> 
+--        case run pa s of 
+--             Failure m   -> Failure m 
+--             Success x c -> Success (f x) c        
+
+instance Applicative (Parser b) where 
     pure x    = success x 
     pf <*> pa = apply pf pa
 
-instance Monad Parser where 
+--instance Applicative Parser where 
+--    pure x    = success x 
+--    pf <*> pa = apply pf pa
+
+instance Monad (Parser b) where 
     pa >>= f = bind pa f 
     pa >> pb = pa *> pb
-    return x = success x 
+    return x = success x
 
-char :: Char -> Parser Char
+--instance Monad Parser where 
+--    pa >>= f = bind pa f 
+--    pa >> pb = pa *> pb
+--    return x = success x 
+
+char :: Char -> Parser String Char
 char x = Parser fn
          where fn []    = Failure "Nothing to match"
                fn (h:t) = if (h == x) 
                           then Success x t 
                           else Failure ("Expected " ++ [x] ++ " but got " ++ [h])  
 
-success :: a -> Parser a 
+success :: a -> Parser b a 
 success x = Parser ( \s -> Success x s )
 
-string :: String -> Parser String
-string x = Parser $
-                     \s -> let n = length x
-                           in  if (x == (take n s)) then 
-                                    Success x (drop n s)
-                               else 
-                                    Failure "Error"
+string :: String -> Parser String String
+string x = Parser $ \s -> 
+    let n = length x
+    in  if   (x == (take n s)) 
+        then Success x (drop n s)
+        else Failure "Error"
 
-digit :: Int -> Parser Int
+digit :: Int -> Parser String Int
 digit x = fmap (\c -> (read c :: Int)) (string (show x))
 
-anyDigit :: Parser Int 
+anyDigit :: Parser String Int 
 anyDigit = (digit 0) <|> (digit 1) <|> (digit 2) <|> (digit 3) <|> (digit 4) <|> 
            (digit 5) <|> (digit 6) <|> (digit 7) <|> (digit 8) <|> (digit 9) 
 
-integer :: Parser Int
+integer :: Parser String Int
 integer = 
         fmap (\l -> (fn l)) (many anyDigit)
         where fn l = read (map (head . show) l) :: Int
 
-label :: String -> Parser a -> Parser a
-label msg pa = 
-        Parser $
-             \s -> case run pa s of 
-                        Failure _ -> Failure msg
-                        x         -> x 
+label :: String -> Parser String a -> Parser String a
+label msg pa = Parser $ \s -> 
+    case run pa s of 
+         Failure _ -> Failure msg
+         x         -> x 
 
-andThen :: Parser a -> Parser b -> Parser (a,b)
+andThen :: Parser String a -> Parser String b -> Parser String (a,b)
 andThen pa pb = pa >>= (\x -> 
                 pb >>= (\y -> 
                 return (x,y)))
 
-alt :: Parser a -> Parser a -> Parser a 
-alt p1 p2 = 
-        Parser $
-             \s -> case run p1 s of
-                        Success m r -> Success m r
-                        Failure _   -> run p2 s
+alt :: Parser String a -> Parser String a -> Parser String a 
+alt p1 p2 = Parser $ \s -> 
+    case run p1 s of
+         Success m r -> Success m r
+         Failure _   -> run p2 s
 
-(<|>) :: Parser a -> Parser a -> Parser a 
+(<|>) :: Parser String a -> Parser String a -> Parser String a 
 (<|>) = alt
 
-slice :: Parser a -> Parser String 
+slice :: Parser String a -> Parser String String 
 slice p = 
         Parser $
              \s ->  case run p s of
@@ -108,12 +131,12 @@ slice p =
                          Success x r -> let n = (length s) - (length r)
                                         in  Success (take n s) r
 
-map2 :: ((a,b) -> c) -> Parser a -> Parser b -> Parser c
+map2 :: ((a,b) -> c) -> Parser String a -> Parser String b -> Parser String c
 map2 f pa pb = pa >>= (\x -> 
                pb >>= (\y -> 
                return $ f (x,y)))
 
-many :: Parser a -> Parser [a]
+many :: Parser String a -> Parser String [a]
 many pa = 
         Parser (\s -> fn [] s)
         where fn accum [] = Success accum [] 
@@ -121,12 +144,12 @@ many pa =
                 Failure _    -> Success accum s
                 Success x r  -> fn (accum ++ [x]) r
  
-many1 :: Parser a -> Parser [a]
+many1 :: Parser String a -> Parser String [a]
 many1 pa = do { first <- pa
                   ; list  <- many pa
                   ; return (first : list)}
 
-exactlyN :: Parser a -> Int -> Parser [a]
+exactlyN :: Parser String a -> Int -> Parser String [a]
 exactlyN pa n = 
         Parser $ 
              \s -> case run (many pa) s of 
@@ -136,35 +159,35 @@ exactlyN pa n =
                                          else Failure "Error"
 
     {-- --}
-bind :: Parser a -> (a -> Parser b) -> Parser b 
+bind :: Parser b a -> (a -> Parser b c) -> Parser b c 
 bind pa f = 
         Parser $
              \s -> case run pa s of
                         Failure msg1  -> Failure msg1
                         Success x1 r1 -> run (f x1) r1 
 
-apply :: Parser (a -> b) -> Parser a -> Parser b
+apply :: Parser b (a -> c) -> Parser b a -> Parser b c
 apply pf pa = pf >>= (\f -> 
               pa >>= (\x -> 
               return $ f x))
 
-failure :: String -> Parser a
+failure :: String -> Parser b a
 failure msg = Parser $ \_ -> Failure msg 
 
 -- Can we make this tail recursive
-anyOf :: String -> Parser Char 
+anyOf :: String -> Parser String Char 
 anyOf (h:t) = (char h) <|> (anyOf t)
 anyOf []    = failure "Could not match any symbols in" 
 
-spaces :: Parser String 
+spaces :: Parser String String 
 spaces = many (char ' ')
 
-anyString :: Parser String 
+anyString :: Parser String String 
 anyString = Parser $ \s -> Success s []
 
     -- Parse until we hit String
     --until :: Parser a -> String -> Parser a 
-choice :: [Parser a] -> Parser a
+choice :: [Parser String a] -> Parser String a
 choice (h:t) = h <|> choice t 
 choice []    = failure "None of the alternatives work"
 
--- src/Tokenizer.hs
+++ src/Tokenizer.hs
@@ -25,55 +25,55 @@ data Token  = COMMA
             | JAL
             deriving (Show, Eq)
 
-commaToken :: Parser Token
+commaToken :: Parser String Token
 commaToken = (char ',') >> return COMMA   
 
-lparenToken :: Parser Token 
+lparenToken :: Parser String Token 
 lparenToken = (char '(') >> return LPAREN
 
-rparenToken :: Parser Token 
+rparenToken :: Parser String Token 
 rparenToken = (char ')') >> return RPAREN
 
-regToken :: Parser Token 
+regToken :: Parser String Token 
 regToken = (char 'x') *> (integer >>= (\i -> return $ REG i))
 
-orToken :: Parser Token 
+orToken :: Parser String Token 
 orToken = (string "or") >> return OR
 
-addToken :: Parser Token 
+addToken :: Parser String Token 
 addToken = (string "add") >> return ADD
 
-subToken :: Parser Token 
+subToken :: Parser String Token 
 subToken = (string "sub") >> return SUB 
 
-lbToken :: Parser Token 
+lbToken :: Parser String Token 
 lbToken = (string "lb") >> return LB 
 
-lhToken :: Parser Token 
+lhToken :: Parser String Token 
 lhToken = (string "lh") >> return LH 
 
-lwToken :: Parser Token 
+lwToken :: Parser String Token 
 lwToken = (string "lw") >> return LW 
 
-ldToken :: Parser Token 
+ldToken :: Parser String Token 
 ldToken = (string "ld") >> return LD 
 
-sbToken :: Parser Token 
+sbToken :: Parser String Token 
 sbToken = (string "sb") >> return SB 
 
-shToken :: Parser Token 
+shToken :: Parser String Token 
 shToken = (string "sh") >> return SH 
 
-swToken :: Parser Token 
+swToken :: Parser String Token 
 swToken = (string "sw") >> return SW 
 
-sdToken :: Parser Token 
+sdToken :: Parser String Token 
 sdToken = (string "sd") >> return SD 
 
-labelToken :: Parser Token 
+labelToken :: Parser String Token 
 labelToken = ((many (anyOf ['a'..'z'])) <* (string ":")) >>= (\s -> return $ LABEL s) 
 
-jalToken :: Parser Token 
+jalToken :: Parser String Token 
 jalToken = (string "jal") >> return JAL 
 
 tokens = choice [ commaToken
@@ -93,7 +93,7 @@ tokens = choice [ commaToken
                 , sdToken
                 , jalToken]
 
-tokenize :: Parser Token -> Parser [Token]
+tokenize :: Parser String Token -> Parser String [Token]
 tokenize p = Parser $ \s -> loop s []
              where loop [] toks = Success toks ""
                    loop s  toks = case run p s of 
